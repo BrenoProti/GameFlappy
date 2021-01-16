e1fa5f304234633a3ed6c2d0d3cf77c6b0d3a647
BrenoProti <moreirasgbr@gmail.com>
2021-01-15 23:00:00 -0300

    Creation of the obstacle
    Creating the obstacles and moving it to the left

---
+++ FlappyGame/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+
+    [SerializeField] private float velocity = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move to left
+        transform.position += Vector3.left * (Time.deltaTime * velocity);
+    }
+}
