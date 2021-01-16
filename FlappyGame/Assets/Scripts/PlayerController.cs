f4f5265c959c32028d321ad50878d5a5b7ffadae
BrenoProti <moreirasgbr@gmail.com>
2021-01-15 22:43:29 -0300

    Player Finished
    Making player fall, fly by pressing spacebar and limiting the speed of fall

---
+++ FlappyGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Geting the Rigidbody 
+    private Rigidbody2D myRB;
+    // speed of fly
+    [SerializeField] private float velocity = 5f;
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Making the player fly by pressing spacebar
+        Fly();
+
+        //limiting the speed of fall
+        LimitingVelocity();
+    }
+
+    //Method to limit the speed of fall
+    private void LimitingVelocity()
+    {
+        if (myRB.velocity.y < -velocity)
+        {
+            myRB.velocity = Vector2.down * velocity;
+        }
+    }
+
+    //Method to make the plane fly
+    public void Fly()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRB.velocity = Vector2.up * velocity;
+        }
+    }
+}
